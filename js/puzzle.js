ee748ae75e2eee9fd42e27240b003c87a414ead9
Gabriel Schmitt <gbs149@outlook.com>
2016-08-15 23:28:28 -0300

    funcional com jQuery animate

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,31 +1,48 @@
 "use strict";
 
 var width = 4;
-var height = 3;
+// var height = 3;
+var tileWidth = "25vw";
+var tileHeight = "33.333vh"
 
 $(".tile").on("click", move);
 
 function move() {
-    var position = Number($(this).css("order"));
-    var emptyPos = Number($("#empty").css("order"));
+    var position = Number($(this).attr("data-pos"));
+    var emptyPos = Number($("#empty").attr("data-pos"));
+    var line = Math.ceil(position / width);
+    var emptyLine = Math.ceil(emptyPos / width);
 
-    if ( position + 1 === emptyPos || 
-         position - 1 === emptyPos ||
-         position + width === emptyPos ||
-         position - width === emptyPos ) {
+    if (position + 1 === emptyPos && line === emptyLine) {
+        $(this).animate({"left": "+=" + tileWidth}, 200)
+        $('#empty').animate({"left": "-=" + tileWidth}, 200);
+        $(this).attr("data-pos", emptyPos);
+        $("#empty").attr("data-pos", position);
+    } 
 
-        /*$(this).css("order", emptyPos);
-        $("#empty").css("order", position);*/
-
-        /*$(this).animate( { order: emptyPos } );
-        $("#empty").animate( { order: position } );*/
-
+    if (position - 1 === emptyPos && line === emptyLine) {
+        $(this).animate({"left": "-=" + tileWidth}, 200)
+        $('#empty').animate({"left": "+=" + tileWidth}, 200);
+        $(this).attr("data-pos", emptyPos);
+        $("#empty").attr("data-pos", position);
     }
-     console.log(position + 1);
 
+    if (position + width === emptyPos) {
+        $(this).animate({"top": "+=" + tileHeight}, 200)
+        $('#empty').animate({"top": "-=" + tileHeight}, 200);
+        $(this).attr("data-pos", emptyPos);
+        $("#empty").attr("data-pos", position);
+    }
     
+    if (position - width === emptyPos) {
+        $(this).animate({"top": "-=" + tileHeight}, 200)
+        $('#empty').animate({"top": "+=" + tileHeight}, 200);
+        $(this).attr("data-pos", emptyPos);
+        $("#empty").attr("data-pos", position);
+    }
+
 }
-                                                                        
+
 
 
 
