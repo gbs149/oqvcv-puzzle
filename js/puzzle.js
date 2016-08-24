4dd1b2df3ee5ba4d45fb16e7bb8ff9702a0d6f08
Gabriel Schmitt <gbs149@outlook.com>
2016-08-24 16:59:50 -0300

    toca audio para cada célula

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,68 +1,69 @@
-"use strict";
+$(document).ready(function () {
+    "use strict";
 
-var width = 4;
-var tileWidth = "25vw";
-var tileHeight = "33.333vh"
+    var width = 4;
+    var tileWidth = "25vw";
+    var tileHeight = "33.333vh"
 
-$(".tile").on("click touchmove", move);
+    $(".tile").on("click touchmove", move);
 
-function move() {
-    var dataPosition = $(this).attr("data-pos");
-    var position = Number(dataPosition);
-    var dataEmptyPosition = $("#empty").attr("data-pos");
-    var emptyPos = Number(dataEmptyPosition);
-    var line = Math.ceil(position / width);
-    var emptyLine = Math.ceil(emptyPos / width);
-    var animateOptions = {
-        duration: 200,
-        start: function() {
-            pauseAudio(dataEmptyPosition);
-        },
-        complete: function() {
-            playAudio(dataPosition);
+    function move() {
+        var dataPosition = $(this).attr("data-pos");
+        var position = Number(dataPosition);
+        var dataEmptyPosition = $("#empty").attr("data-pos");
+        var emptyPos = Number(dataEmptyPosition);
+        var line = Math.ceil(position / width);
+        var emptyLine = Math.ceil(emptyPos / width);
+        var animateOptions = {
+            duration: 200,
+            start: function () {
+                pauseAudio(dataEmptyPosition);
+            },
+            complete: function () {
+                playAudio(dataPosition);
+            }
+        };
+
+        if (position + 1 === emptyPos && line === emptyLine) {
+            $(this).animate({ "left": "+=" + tileWidth }, animateOptions)
+            $('#empty').animate({ "left": "-=" + tileWidth }, animateOptions);
+            $(this).attr("data-pos", emptyPos);
+            $("#empty").attr("data-pos", position);
         }
+
+        if (position - 1 === emptyPos && line === emptyLine) {
+            $(this).animate({ "left": "-=" + tileWidth }, animateOptions)
+            $('#empty').animate({ "left": "+=" + tileWidth }, animateOptions);
+            $(this).attr("data-pos", emptyPos);
+            $("#empty").attr("data-pos", position);
+        }
+
+        if (position + width === emptyPos) {
+            $(this).animate({ "top": "+=" + tileHeight }, animateOptions)
+            $('#empty').animate({ "top": "-=" + tileHeight }, animateOptions);
+            $(this).attr("data-pos", emptyPos);
+            $("#empty").attr("data-pos", position);
+        }
+
+        if (position - width === emptyPos) {
+            $(this).animate({ "top": "-=" + tileHeight }, animateOptions)
+            $('#empty').animate({ "top": "+=" + tileHeight }, animateOptions);
+            $(this).attr("data-pos", emptyPos);
+            $("#empty").attr("data-pos", position);
+        }
+
+    }
+
+    function playAudio(id) {
+        var audio = document.getElementById(id);
+        audio.load();
+        audio.play();
+    }
+
+    function pauseAudio(id) {
+        var audio = document.getElementById(id);
+        audio.pause();
     };
-
-    if (position + 1 === emptyPos && line === emptyLine) {
-        $(this).animate({"left": "+=" + tileWidth}, animateOptions)
-        $('#empty').animate({"left": "-=" + tileWidth}, animateOptions);
-        $(this).attr("data-pos", emptyPos);
-        $("#empty").attr("data-pos", position);
-    } 
-
-    if (position - 1 === emptyPos && line === emptyLine) {
-        $(this).animate({"left": "-=" + tileWidth}, animateOptions)
-        $('#empty').animate({"left": "+=" + tileWidth}, animateOptions);
-        $(this).attr("data-pos", emptyPos);
-        $("#empty").attr("data-pos", position);
-    }
-
-    if (position + width === emptyPos) {
-        $(this).animate({"top": "+=" + tileHeight}, animateOptions)
-        $('#empty').animate({"top": "-=" + tileHeight}, animateOptions);
-        $(this).attr("data-pos", emptyPos);
-        $("#empty").attr("data-pos", position);
-    }
-    
-    if (position - width === emptyPos) {
-        $(this).animate({"top": "-=" + tileHeight}, animateOptions)
-        $('#empty').animate({"top": "+=" + tileHeight}, animateOptions);
-        $(this).attr("data-pos", emptyPos);
-        $("#empty").attr("data-pos", position);
-    }
-
-}
-
-function playAudio(id) {
-    var audio = document.getElementById(id);
-    audio.load();
-    audio.play();
-}
-
-function pauseAudio(id) {
-    var audio = document.getElementById(id);
-    audio.pause();
-};
-
+});
 
 
