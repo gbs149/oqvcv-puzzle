bba3eeaac3ee2b5f31c71fd56ce7227f5f269a01
Gabriel Schmitt <gbs149@outlook.com>
2016-09-10 08:42:54 -0300

    disable click handler during animation

--- js/puzzle.js
+++ js/puzzle.js
@@ -4,61 +4,67 @@ $(document).ready(function () {
     var colunas = 4;
     var tileWidth = "480px";
     var tileHeight = "360px";
+    var emptyPosition = 8;
 
     var timer;
 
-    $(".tile").on("click touchmove", function() {
+    $(".tile").on("click", clickHandler);
+
+    function clickHandler() {
         move($(this));
-        console.log(timer);
+        // $(this).off();
         clearTimeout(timer);
+        console.log(emptyPosition);
         timer = setTimeout(function () {
             window.location.reload();
         }, 5 * 60000);
-    });
+    }
 
     function move($obj) {
         var $empty = $("#empty");
         var position = Number($obj.attr("data-pos"));
-        var emptyPosition = Number($empty.attr("data-pos"));
+        // var emptyPosition = Number($empty.attr("data-pos"));
         var line = Math.ceil(position / colunas);
         var emptyLine = Math.ceil(emptyPosition / colunas);
         
         var animateOptions = {
-            duration: 200,
+            duration: 1200,
             start: function () {
+                $obj.off("click", clickHandler);
                 pauseAudio(emptyPosition);
             },
             complete: function () {
                 playAudio(position);
+                $obj.on("click", clickHandler);
             }
         };
 
         if (position + 1 === emptyPosition && line === emptyLine) {
             $obj.animate({ "left": "+=" + tileWidth }, animateOptions)
-            $empty.animate({ "left": "-=" + tileWidth }, animateOptions);
+            // $empty.animate({ "left": "-=" + tileWidth }, animateOptions);
             $obj.attr("data-pos", emptyPosition);
-            $empty.attr("data-pos", position);
+            emptyPosition = position;
         }
 
         if (position - 1 === emptyPosition && line === emptyLine) {
             $obj.animate({ "left": "-=" + tileWidth }, animateOptions)
-            $empty.animate({ "left": "+=" + tileWidth }, animateOptions);
+            // $empty.animate({ "left": "+=" + tileWidth }, animateOptions);
             $obj.attr("data-pos", emptyPosition);
-            $empty.attr("data-pos", position);
+            emptyPosition = position;
         }
 
         if (position + colunas === emptyPosition) {
             $obj.animate({ "top": "+=" + tileHeight }, animateOptions)
-            $empty.animate({ "top": "-=" + tileHeight }, animateOptions);
+            // $empty.animate({ "top": "-=" + tileHeight }, animateOptions);
             $obj.attr("data-pos", emptyPosition);
-            $empty.attr("data-pos", position);
+            emptyPosition = position;
         }
 
         if (position - colunas === emptyPosition) {
             $obj.animate({ "top": "-=" + tileHeight }, animateOptions)
-            $empty.animate({ "top": "+=" + tileHeight }, animateOptions);
+            // $empty.animate({ "top": "+=" + tileHeight }, animateOptions);
             $obj.attr("data-pos", emptyPosition);
-            $empty.attr("data-pos", position);
+            emptyPosition = position;
         }
 
     }
