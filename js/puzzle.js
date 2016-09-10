704b60103e7213b2a5e5cb7228253ba1d489c648
Gabriel Schmitt <gbs149@outlook.com>
2016-09-10 08:55:37 -0300

    comments

--- js/puzzle.js
+++ js/puzzle.js
@@ -4,29 +4,32 @@ $(document).ready(function () {
     var colunas = 4;
     var tileWidth = "480px";
     var tileHeight = "360px";
-    var emptyPosition = 8;
+    var emptyPosition = 8; // posição inicial
 
-    var timer;
+    var timer; // timer para recarregar a página
 
     $(".tile").on("click", clickHandler);
 
     function clickHandler() {
         move($(this));
+
+        // reseta o timer a cada click
         clearTimeout(timer);
-        console.log(emptyPosition);
+
+        // em X minutos sem atividade, recarrega a página
         timer = setTimeout(function () {
             window.location.reload();
         }, 5 * 60000);
     }
 
+    // função que move as peças e aciona o áudio ao final da animação
     function move($obj) {
-        var $empty = $("#empty");
         var position = Number($obj.attr("data-pos"));
         var line = Math.ceil(position / colunas);
         var emptyLine = Math.ceil(emptyPosition / colunas);
         
         var animateOptions = {
-            duration: 1200,
+            duration: 600,
             start: function () {
                 $obj.off("click", clickHandler);
                 pauseAudio(emptyPosition);
@@ -37,6 +40,8 @@ $(document).ready(function () {
             }
         };
 
+        // verifica se a peça clicada é adjacente à posição vazia (linha ou coluna)
+        // se for, move a peça na direção correta
         if (position + 1 === emptyPosition && line === emptyLine) {
             $obj.animate({ "left": "+=" + tileWidth }, animateOptions)
             $obj.attr("data-pos", emptyPosition);
@@ -63,12 +68,14 @@ $(document).ready(function () {
 
     }
 
+    // toca o elemento de áudio. usada ao final da animação.
     function playAudio(id) {
         var audio = document.getElementById(id);
         audio.load();
         audio.play();
     }
 
+    // para a reprodução do áudio. Usada em caso de novo click antes do fim do áudio.
     function pauseAudio(id) {
         var audio = document.getElementById(id);
         audio.pause();
