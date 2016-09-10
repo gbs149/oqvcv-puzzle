4edcf927135390ada06334d5c30a4abd6c8a5062
Gabriel Schmitt <gbs149@outlook.com>
2016-09-09 21:46:04 -0300

    timer para recarregar a página

--- js/puzzle.js
+++ js/puzzle.js
@@ -3,18 +3,105 @@ $(document).ready(function () {
 
     var colunas = 4;
     var tileWidth = "480px";
-    var tileHeight = "360px"
+    var tileHeight = "360px";
 
-    $(".tile").on("click touchmove", move);
+    var timer;
 
-    function move() {
-        var dataPosition = $(this).attr("data-pos");
+    $(".tile").on("click touchmove", function() {
+        move($(this));
+        console.log(timer);
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            window.location.reload();
+        }, 5 * 60000);
+    });
+
+    function move($obj) {
+        var $empty = $("#empty");
+        var position = Number($obj.attr("data-pos"));
+        var emptyPosition = Number($empty.attr("data-pos"));
+        var line = Math.ceil(position / colunas);
+        var emptyLine = Math.ceil(emptyPosition / colunas);
+        
+        var animateOptions = {
+            duration: 200,
+            start: function () {
+                pauseAudio(emptyPosition);
+            },
+            complete: function () {
+                playAudio(position);
+            }
+        };
+
+        if (position + 1 === emptyPosition && line === emptyLine) {
+            $obj.animate({ "left": "+=" + tileWidth }, animateOptions)
+            $empty.animate({ "left": "-=" + tileWidth }, animateOptions);
+            $obj.attr("data-pos", emptyPosition);
+            $empty.attr("data-pos", position);
+        }
+
+        if (position - 1 === emptyPosition && line === emptyLine) {
+            $obj.animate({ "left": "-=" + tileWidth }, animateOptions)
+            $empty.animate({ "left": "+=" + tileWidth }, animateOptions);
+            $obj.attr("data-pos", emptyPosition);
+            $empty.attr("data-pos", position);
+        }
+
+        if (position + colunas === emptyPosition) {
+            $obj.animate({ "top": "+=" + tileHeight }, animateOptions)
+            $empty.animate({ "top": "-=" + tileHeight }, animateOptions);
+            $obj.attr("data-pos", emptyPosition);
+            $empty.attr("data-pos", position);
+        }
+
+        if (position - colunas === emptyPosition) {
+            $obj.animate({ "top": "-=" + tileHeight }, animateOptions)
+            $empty.animate({ "top": "+=" + tileHeight }, animateOptions);
+            $obj.attr("data-pos", emptyPosition);
+            $empty.attr("data-pos", position);
+        }
+
+    }
+
+    function playAudio(id) {
+        var audio = document.getElementById(id);
+        audio.load();
+        audio.play();
+    }
+
+    function pauseAudio(id) {
+        var audio = document.getElementById(id);
+        audio.pause();
+    };
+});
+
+
+/*
+
+$(document).ready(function () {
+    "use strict";
+
+    var colunas = 4;
+    var tileWidth = "480px",
+        tileHeight = "360px";
+    var timer;
+
+    $(".tile").on("click touchmove", function () {
+        move($(this).attr("data-pos"));
+        console.log($(this).attr("data-pos"))
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            window.location.reload(false);
+        }, 5 * 1000);
+    });
+
+    function move(dataPosition) {
         var position = Number(dataPosition);
         var dataEmptyPosition = $("#empty").attr("data-pos");
         var emptyPos = Number(dataEmptyPosition);
         var line = Math.ceil(position / colunas);
         var emptyLine = Math.ceil(emptyPos / colunas);
-        
+
         var animateOptions = {
             duration: 200,
             start: function () {
@@ -66,5 +153,4 @@ $(document).ready(function () {
         audio.pause();
     };
 });
-
-
+*/
